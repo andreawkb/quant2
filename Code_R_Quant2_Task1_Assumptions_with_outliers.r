8b745a82413d9442407794e480ffb4a9c9ca0d89
andreawkb <65731558+andreawkb@users.noreply.github.com>
2020-05-21 20:39:20 +0100

    Add files via upload

---
+++ Code_R_Quant2_Task1_Assumptions_with_outliers.r
@@ -0,0 +1,53 @@
+#Import cleaned dataset with outliers
+
+df = read.csv("task1_data_clean_ready.csv")
+print(df)
+
+reg = lm(GovSupport~NegEmot, data=df)
+
+plot(reg)
+
+#Test for homoscedasticity
+
+plot(reg, which= c(3))
+
+
+#Test for normality of residuals
+
+plot(reg, which=2)
+
+summary(reg)
+
+#Multicollinearity test, create dataframe with the three independent variables to run diagnostics
+
+df2 = data.frame(df$NegEmot, df$Egalitarianism, df$Individualism)
+
+#Run correlation matrix first of all
+
+cor(df2)
+
+#Correlation looks OK, everything <0.8. Now need to run multiple regression model first before I can run the other multicollinearity diagnostics. 
+
+reg2 = lm(GovSupport~NegEmot+Egalitarianism+Individualism, data=df)
+summary(reg2)
+
+#Check assumptions for 2nd regression model
+
+plot(reg2, which= c(3))
+
+plot(reg2, which=2)
+
+#Check assumptions for 3rd regression model
+
+reg3 = lm(GovSupport~NegEmot+Egalitarianism+Individualism+AGE+GENDER, data=df)
+summary(reg3)
+
+#Check multicollinearity for reg3 independent variables
+
+df3 = data.frame(df$NegEmot, df$Egalitarianism, df$Individualism, df$GENDER, df$AGE)
+
+cor(df3)
+
+plot(reg3, which=2)
+
+plot(reg3, which= c(3))
